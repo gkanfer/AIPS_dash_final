4836829d68569b709145f8086f02f19e29d66c12
gkanfer <43918436+gkanfer@users.noreply.github.com>
2022-02-15 07:21:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import dash
 import dash_bootstrap_components as dbc
+import diskcache
+from dash.long_callback import DiskcacheLongCallbackManager
+
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
 
 # meta_tags are required for the app layout to be mobile responsive
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}],
-                external_stylesheets=[dbc.themes.JOURNAL]
+                external_stylesheets=[dbc.themes.JOURNAL],
+                long_callback_manager=long_callback_manager
                 )
 server = app.server
